c68c5cb15806fa0ac1008fb2c2e6fa91bfd57ed6
DevArthurSantos <arthursantos.s@hotmail.com>
2023-06-05 14:34:51 -0300

    add: AboutSection in the Layout

--- src/foundations/layout/Home/index.tsx
+++ src/foundations/layout/Home/index.tsx
@@ -1,12 +1,14 @@
 import S from "./styled"
 import Header from "@foundations/components/patterns/Header";
 import HomeSection from "@foundations/components/shared/HomeSection";
+import AboutSection from "@foundations/components/shared/AboutSection";
 
 function HomePageLayout(): JSX.Element {
   return (
     <S.Container>
       <Header />
       <HomeSection />
+      <AboutSection />
     </S.Container>
   );
 }
