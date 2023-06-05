4a255c283a4aa72e8b09104f23b649786eee4557
DevArthurSantos <arthursantos.s@hotmail.com>
2023-06-05 14:37:18 -0300

    feat: add a new seciton 'About'

---
+++ src/foundations/components/shared/AboutSection/styled.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const Styleds = {
+  AboutSectionWrapper: styled.section`
+    width: 100%;
+  `,
+  AboutSectionContext: styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-flow: row wrap;
+  height: 100vh;
+  `,
+  TextContext: styled.div`
+  width: 552px;
+  height: 513px;
+  background: 'red';
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column nowrap;
+  gap: 15px;
+  `,
+  ImageContainer: styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 552px;
+  height: 513px;
+  `,
+  Image: styled.img`
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 15px;
+  `
+};
+
+export default Styleds;
